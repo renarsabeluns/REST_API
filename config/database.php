6c009beaf0c54b92d4330b56cfa7e72516ea81a1
Renārs <renars.abeluns@Inbox.lv>
2022-03-14 19:28:13 +0200

    Setup db connection

---
+++ config/database.php
@@ -0,0 +1,30 @@
+<?php
+
+class Database {
+    //Database parameters
+    private $host = 'loclahost';
+    private $db_name = 'restapi';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    //Database connection method
+
+    public function connect(){
+        $this -> conn = null;
+
+        try{
+            $this -> conn = new PDO('mysql:host=' . $this->host . 'dbname=' . $this->db_name,
+        $this->username, $this->password);
+
+        $this -> conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch(PDOException $error){
+            echo 'Connection Error'. $error ->getMessage();
+
+        }
+        return $this -> conn;
+    }
+
+}
+?>
